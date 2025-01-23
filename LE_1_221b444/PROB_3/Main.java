bd6ed74d3551e7ed5cfa232d709f127daebb277a
Vinit Choudhary <vinitchoudhary2004@gmail.com>
2025-01-23 18:16:56 +0530

    Update Main.java

--- LE_1_221b444/PROB_3/Main.java
+++ LE_1_221b444/PROB_3/Main.java
@@ -1,4 +1,4 @@
-class Mainhttps://www.onlinegdb.com/edit/vP3Olb1eZf#editor_2 {
+class Main {
  public static void main (String args[]){
  Mother m= new Mother ( );
  //m.show( ); // show of Mother is called
